9e47f98a28289edc5cd563cb905a1db53e03aeac
karthik-pro-engr <101930095+karthik-pro-engr@users.noreply.github.com>
2026-02-01 16:10:56 +0530

    feat(data): add GithubService.kt for fetching repos from API

---
+++ data/src/main/java/com/karthik/pro/engr/github/api/data/network/GithubService.kt
@@ -0,0 +1,15 @@
+package com.karthik.pro.engr.github.api.data.network
+
+import com.karthik.pro.engr.github.api.data.network.dto.GitHubRepoDto
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface GithubService {
+    @GET("users/{userame}/repos")
+    suspend fun listUserRepos(
+        @Path("username") username: String,
+        @Query("per_page") perPage: Int = 30,
+        @Query("page") page: Int = 1
+    ): List<GitHubRepoDto>
+}
