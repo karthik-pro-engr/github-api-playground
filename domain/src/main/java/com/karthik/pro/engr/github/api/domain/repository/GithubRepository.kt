691df76d7ea9b626666949a281e2184bc3e17544
karthik-pro-engr <101930095+karthik-pro-engr@users.noreply.github.com>
2026-02-01 16:35:59 +0530

    fix(domain): update suspend function for getUserRepos

--- domain/src/main/java/com/karthik/pro/engr/github/api/domain/repository/GithubRepository.kt
+++ domain/src/main/java/com/karthik/pro/engr/github/api/domain/repository/GithubRepository.kt
@@ -1,8 +1,9 @@
 package com.karthik.pro.engr.github.api.domain.repository
 
 import com.karthik.pro.engr.github.api.domain.model.Repo
+import kotlinx.coroutines.flow.Flow
 
 
 interface GithubRepository {
-    fun getUserRepos(username: String, perPage: Int = 30, page: Int = 1): Flow<List<Repo>>
+    suspend fun getUserRepos(username: String, perPage: Int = 30, page: Int = 1): Flow<List<Repo>>
 }
