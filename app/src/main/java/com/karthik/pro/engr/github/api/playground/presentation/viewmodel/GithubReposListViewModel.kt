dd2f6ffc098e78b0e3f0a08ad92ad8f895e4ea0d
karthik-pro-engr <101930095+karthik-pro-engr@users.noreply.github.com>
2026-02-03 16:36:47 +0530

    fix(domain): add GithubReposListViewModel to handle repo loading result

---
+++ app/src/main/java/com/karthik/pro/engr/github/api/playground/presentation/viewmodel/GithubReposListViewModel.kt
@@ -0,0 +1,45 @@
+package com.karthik.pro.engr.github.api.playground.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.karthik.pro.engr.github.api.domain.error.DomainError
+import com.karthik.pro.engr.github.api.domain.model.Repo
+import com.karthik.pro.engr.github.api.domain.result.Result
+import com.karthik.pro.engr.github.api.domain.usecase.GetUserReposUseCase
+import com.karthik.pro.engr.github.api.playground.presentation.ui.state.UiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class GithubReposListViewModel @Inject constructor(private val useCase: GetUserReposUseCase) :
+    ViewModel() {
+
+    private var _uiState = MutableStateFlow<UiState<List<Repo>>>(UiState.Idle)
+    val uiState: StateFlow<UiState<List<Repo>>> = _uiState
+
+    fun loadRepos(username: String) {
+        viewModelScope.launch {
+            useCase(username).collect { result ->
+                _uiState.value = when (result) {
+                    is Result.Failure -> UiState.Error(mapError(result.error))
+                    is Result.Success -> UiState.Success(result.data)
+                }
+            }
+        }
+
+    }
+
+    private fun mapError(error: DomainError): String {
+        return when (error) {
+            DomainError.Network -> "No internet connection"
+            DomainError.Unauthorized -> "Unauthorized access"
+            DomainError.NotFound -> "User not found"
+            DomainError.RateLimited -> "Rate limit exceeded"
+            DomainError.Unknown -> "Something went wrong"
+        }
+    }
+
+}
