3cba2a732ea493f8d3226ed4d7e57ca7a5de32a6
karthik-pro-engr <101930095+karthik-pro-engr@users.noreply.github.com>
2026-02-03 08:42:41 +0530

    feat(app): add Hilt GithubPlaygroundApp and update with AndroidManifest

---
+++ app/src/main/java/com/karthik/pro/engr/github/api/playground/app/GithubPlaygroundApp.kt
@@ -0,0 +1,8 @@
+package com.karthik.pro.engr.github.api.playground.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class GithubPlaygroundApp: Application() {
+}
