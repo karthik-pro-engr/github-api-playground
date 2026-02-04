2cb26a113f736520f93a73994ed8fa43bcca0c2a
karthik-pro-engr <101930095+karthik-pro-engr@users.noreply.github.com>
2026-02-04 21:51:51 +0530

    feat(app): add RepoListRoute to connect ViewModel with UI

---
+++ app/src/main/java/com/karthik/pro/engr/github/api/playground/presentation/repos/RepoListRoute.kt
@@ -0,0 +1,21 @@
+package com.karthik.pro.engr.github.api.playground.presentation.repos
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import com.karthik.pro.engr.github.api.playground.presentation.viewmodel.GithubReposListViewModel
+
+@Composable
+fun RepoListRoute(
+    modifier: Modifier = Modifier,
+    viewModel: GithubReposListViewModel = hiltViewModel()
+) {
+    val state by viewModel.uiState.collectAsState()
+    RepoListScreen(
+        modifier = modifier,
+        state = state,
+        onFetch = viewModel::loadRepos
+    )
+}
