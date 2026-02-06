ec9eec8668bb4622446948ab7e7e348db45e431d
karthik-pro-engr <101930095+karthik-pro-engr@users.noreply.github.com>
2026-02-06 13:54:24 +0530

    fix(data): add android log util

---
+++ data/src/test/java/android/util/Log.java
@@ -0,0 +1,10 @@
+package android.util;
+
+public final class Log {
+    public static boolean isLoggable(String tag, int level) { return false; }
+    public static int d(String tag, String msg) { return 0; }
+    public static int e(String tag, String msg) { return 0; }
+    public static int w(String tag, String msg) { return 0; }
+    public static int i(String tag, String msg) { return 0; }
+    public static int v(String tag, String msg) { return 0; }
+}
