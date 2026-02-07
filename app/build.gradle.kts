a59b0acffcf5a309e532c98bca21ecc03c07f2d4
karthik-pro-engr <101930095+karthik-pro-engr@users.noreply.github.com>
2026-02-07 13:24:25 +0530

    chore(app-deps): add truth dependecy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,6 +87,7 @@ dependencies {
     testImplementation(libs.junit)
     testImplementation(libs.mockk)
     testImplementation(libs.turbine)
+    testImplementation(libs.truth)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
