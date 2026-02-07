601c3192c0ff202aeaea17166664b946fd30c87b
karthik-pro-engr <101930095+karthik-pro-engr@users.noreply.github.com>
2026-02-07 11:29:08 +0530

    chore(data): add mockK and turbine dependencies

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -57,6 +57,8 @@ dependencies {
 
     testImplementation(libs.junit)
     testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.mockk)
+    testImplementation(libs.turbine)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     testImplementation(libs.okhttp.mockwebserver)
