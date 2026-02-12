9ff0edd89aaec96fa3b4b4ec1750f7729373b0af
karthik-pro-engr <101930095+karthik-pro-engr@users.noreply.github.com>
2026-02-12 19:18:53 +0530

    feat(domain): update TokenStorage with observe method

--- domain/src/main/java/com/karthik/pro/engr/github/api/domain/security/TokenStorage.kt
+++ domain/src/main/java/com/karthik/pro/engr/github/api/domain/security/TokenStorage.kt
@@ -1,7 +1,10 @@
 package com.karthik.pro.engr.github.api.domain.security
 
+import kotlinx.coroutines.flow.Flow
+
 interface TokenStorage {
     suspend fun save(token: String)
-    suspend fun read(): String
+    suspend fun read(): String?
     suspend fun clear()
+     fun observe(): Flow<String?>
 }
