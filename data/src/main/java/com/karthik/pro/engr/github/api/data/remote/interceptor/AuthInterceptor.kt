ed5ff2434c5f8a5874f6bf56aaf0a4dc13af14f4
karthik-pro-engr <101930095+karthik-pro-engr@users.noreply.github.com>
2026-02-12 19:27:13 +0530

    feat(auth): add AuthInterceptor to add header

---
+++ data/src/main/java/com/karthik/pro/engr/github/api/data/remote/interceptor/AuthInterceptor.kt
@@ -0,0 +1,24 @@
+package com.karthik.pro.engr.github.api.data.remote.interceptor
+
+import com.karthik.pro.engr.github.api.data.di.security.TokenProvider
+import okhttp3.Interceptor
+import okhttp3.Response
+import javax.inject.Inject
+
+class AuthInterceptor @Inject constructor(private val tokenProvider: TokenProvider) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+        val token = tokenProvider.getTokenSync()
+        return chain.proceed(
+            if (token.isNullOrEmpty()) {
+                originalRequest
+            } else {
+                originalRequest
+                    .newBuilder()
+                    .header("Authorization", "Bearer $token")
+                    .header("Accept", "application/vnd.github+json")
+                    .build()
+            }
+        )
+    }
+}
