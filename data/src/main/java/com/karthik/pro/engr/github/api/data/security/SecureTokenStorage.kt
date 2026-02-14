48c419092c9e9c025efb25b3e35735151747c1a7
karthik-pro-engr <101930095+karthik-pro-engr@users.noreply.github.com>
2026-02-14 19:46:25 +0530

    refactor(data): update Android Base64 to Java Base64 to support unit testing

--- data/src/main/java/com/karthik/pro/engr/github/api/data/security/SecureTokenStorage.kt
+++ data/src/main/java/com/karthik/pro/engr/github/api/data/security/SecureTokenStorage.kt
@@ -1,8 +1,6 @@
 package com.karthik.pro.engr.github.api.data.security
 
-import android.util.Base64
-import android.util.Base64.decode
-import androidx.datastore.preferences.core.stringPreferencesKey
+
 import com.google.crypto.tink.Aead
 import com.karthik.pro.engr.github.api.data.security.config.PreferenceKeys.ENCRYPTED_GITHUB_TOKEN
 import com.karthik.pro.engr.github.api.domain.security.TokenStorage
@@ -11,6 +9,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
 import java.nio.charset.Charset
+import java.util.Base64
 import javax.inject.Inject
 
 
@@ -24,7 +23,7 @@ class SecureTokenStorage @Inject constructor(
     override suspend fun save(token: String) {
         withContext(Dispatchers.IO) {
             val encrypt = aead.encrypt(token.toByteArray(), null)
-            val encoded = Base64.encodeToString(encrypt, Base64.NO_WRAP)
+            val encoded = Base64.getEncoder().encodeToString(encrypt)
             dataStoreKeyValueStore.putString(ENCRYPTED_GITHUB_TOKEN, encoded)
 
         }
@@ -37,7 +36,7 @@ class SecureTokenStorage @Inject constructor(
             return@withContext null
         }
         return@withContext try {
-            val encrypted = decode(encoded, Base64.NO_WRAP)
+            val encrypted = Base64.getDecoder().decode(encoded)
             val decrypt = aead.decrypt(encrypted, null)
             String(decrypt, charset)
         } catch (e: Exception) {
@@ -58,7 +57,7 @@ class SecureTokenStorage @Inject constructor(
                 return@map null
             }
             try {
-                val decoded = decode(encoded, Base64.NO_WRAP)
+                val decoded = Base64.getDecoder().decode(encoded)
                 val decrypt = aead.decrypt(decoded, null)
                 String(decrypt, charset)
             } catch (t: Throwable) {
