e1e846117d91558f2bdcc05ab4858e4de901e62b
karthik-pro-engr <101930095+karthik-pro-engr@users.noreply.github.com>
2026-02-14 09:17:20 +0530

    refactor(data): update KeyValueStore with remove function

--- data/src/main/java/com/karthik/pro/engr/github/api/data/security/KeyValueStore.kt
+++ data/src/main/java/com/karthik/pro/engr/github/api/data/security/KeyValueStore.kt
@@ -2,8 +2,11 @@ package com.karthik.pro.engr.github.api.data.security
 
 import kotlinx.coroutines.flow.Flow
 
-interface TokenKeyValueStore {
+interface KeyValueStore {
     suspend fun getString(key: String): String?
     suspend fun putString(key: String, value: String)
+
+    suspend fun remove(key:String)
+
     fun observeString(key: String): Flow<String?>
 }
