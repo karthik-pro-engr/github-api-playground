35d6f45e55ad6486f3e7fbbcb970fc27ac7c5b03
karthik-pro-engr <101930095+karthik-pro-engr@users.noreply.github.com>
2026-03-07 15:52:01 +0530

    Revert "feat(auth): authentication with github token is in progress"
    This reverts commit 332446b2

--- app/src/main/java/com/karthik/pro/engr/github/api/playground/presentation/repos/GithubReposListViewModel.kt
+++ app/src/main/java/com/karthik/pro/engr/github/api/playground/presentation/repos/GithubReposListViewModel.kt
@@ -1,7 +1,10 @@
 package com.karthik.pro.engr.github.api.playground.presentation.repos
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.karthik.pro.engr.github.api.data.di.security.TokenProvider
+import com.karthik.pro.engr.github.api.data.security.SecureTokenStorage
 import com.karthik.pro.engr.github.api.domain.error.DomainError
 import com.karthik.pro.engr.github.api.domain.model.Repo
 import com.karthik.pro.engr.github.api.domain.result.Result
@@ -13,7 +16,11 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class GithubReposListViewModel @Inject constructor(private val useCase: GetUserReposUseCase) :
+class GithubReposListViewModel @Inject constructor(
+    private val useCase: GetUserReposUseCase,
+    private val secureTokenStorage: SecureTokenStorage,
+    private val tokenProvider: TokenProvider
+) :
     ViewModel() {
 
     private var _uiState = MutableStateFlow<UiState<List<Repo>>>(UiState.Idle)
@@ -22,6 +29,12 @@ class GithubReposListViewModel @Inject constructor(private val useCase: GetUserR
     fun loadRepos(username: String) {
         viewModelScope.launch {
             _uiState.value = UiState.Loading
+            val token = tokenProvider.getTokenSync()
+            if (token.isNullOrEmpty()) {
+                secureTokenStorage.save("place_holder")
+            } else {
+                Log.i("ViewModel", "loadRepos: savedToken-> $token")
+            }
             useCase(username).collect { result ->
                 _uiState.value = when (result) {
                     is Result.Failure -> UiState.Error(mapError(result.error))
