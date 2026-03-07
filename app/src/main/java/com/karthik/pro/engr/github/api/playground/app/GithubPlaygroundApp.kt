331dffb5e3d9ae91f960e09a5d0156b60f3f4d2c
karthik-pro-engr <101930095+karthik-pro-engr@users.noreply.github.com>
2026-03-07 16:09:25 +0530

    feat(work-manager): add hilt worker factory

--- app/src/main/java/com/karthik/pro/engr/github/api/playground/app/GithubPlaygroundApp.kt
+++ app/src/main/java/com/karthik/pro/engr/github/api/playground/app/GithubPlaygroundApp.kt
@@ -2,14 +2,19 @@ package com.karthik.pro.engr.github.api.playground.app
 
 import android.app.Application
 import android.util.Log
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import com.google.firebase.FirebaseApp
 import com.karthik.pro.engr.github.api.playground.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class GithubPlaygroundApp : Application() {
+class GithubPlaygroundApp : Application(), Configuration.Provider {
+
     override fun onCreate() {
         super.onCreate()
+
         if (BuildConfig.ENABLE_APP_DISTRIBUTION) {
             setupBetaFeatures()
         }
@@ -33,4 +38,13 @@ class GithubPlaygroundApp : Application() {
             BetaAppFeedbackController(vm, BetaAppFeedbackActions(vm))
         }*/
     }
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+
 }
