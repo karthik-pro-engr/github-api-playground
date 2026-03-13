f30dfce38a778e3c2571e71708eb4ebdfe1f5f78
karthik-pro-engr <101930095+karthik-pro-engr@users.noreply.github.com>
2026-03-13 07:44:12 +0530

    refactor(api): return Response<List<GitHubRepoDto>> to support pagination headers

--- data/src/main/java/com/karthik/pro/engr/github/api/data/remote/api/GithubService.kt
+++ data/src/main/java/com/karthik/pro/engr/github/api/data/remote/api/GithubService.kt
@@ -1,6 +1,7 @@
 package com.karthik.pro.engr.github.api.data.remote.api
 
 import com.karthik.pro.engr.github.api.data.remote.dto.GitHubRepoDto
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -11,5 +12,5 @@ interface GithubService {
         @Path("username") username: String,
         @Query("per_page") perPage: Int = 30,
         @Query("page") page: Int = 1
-    ): List<GitHubRepoDto>
+    ): Response<List<GitHubRepoDto>>
 }
