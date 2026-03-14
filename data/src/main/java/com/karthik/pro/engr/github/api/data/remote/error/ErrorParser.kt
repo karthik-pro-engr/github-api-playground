f50699adc66dc6c922bbbab5aecd22c63d66b074
karthik-pro-engr <101930095+karthik-pro-engr@users.noreply.github.com>
2026-03-14 22:32:38 +0530

    feat(repos-pagination): add ErrorParser.kt for parsing the error response

---
+++ data/src/main/java/com/karthik/pro/engr/github/api/data/remote/error/ErrorParser.kt
@@ -0,0 +1,29 @@
+package com.karthik.pro.engr.github.api.data.remote.error
+
+import com.karthik.pro.engr.github.api.data.remote.dto.error.ErrorDto
+import retrofit2.Response
+import retrofit2.Retrofit
+import javax.inject.Inject
+
+class ErrorParser @Inject constructor(private val retrofit: Retrofit) {
+    private val converter =
+        retrofit.responseBodyConverter<ErrorDto>(ErrorDto::class.java, arrayOf())
+
+    fun parse(response: Response<*>): ErrorDto {
+        val errorBody = response.errorBody()
+        if (errorBody != null) {
+            try {
+                converter.convert(errorBody)
+            } catch (t: Throwable) {
+                try {
+                    val raw = errorBody.string()
+                    return ErrorDto(raw)
+                } catch (_: Throwable) {
+                }
+            }
+        }
+
+        return ErrorDto(response.message())
+
+    }
+}
