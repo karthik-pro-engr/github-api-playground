5ef9e9f50591817d0cbfadcaa9fc58f6f9233749
karthik-pro-engr <101930095+karthik-pro-engr@users.noreply.github.com>
2026-03-16 06:57:02 +0530

    refactor(data): introduce STARTING_PAGE_INDEX and move DEFAULT_PAGE_SIZE to domain
    - Remove DEFAULT_PAGE_SIZE from NetworkConstants
    - Move pagination size constant to domain module
    - Add STARTING_PAGE_INDEX for PagingSource initial page

--- data/src/main/java/com/karthik/pro/engr/github/api/data/remote/constants/NetworkConstants.kt
+++ data/src/main/java/com/karthik/pro/engr/github/api/data/remote/constants/NetworkConstants.kt
@@ -2,5 +2,5 @@ package com.karthik.pro.engr.github.api.data.remote.constants
 
 object NetworkConstants {
     const val HEADER_LINK = "Link"
-    const val DEFAULT_PAGE_SIZE = 15
+    const val STARTING_PAGE_INDEX = 1
 }
