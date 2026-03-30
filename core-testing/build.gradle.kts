2717f520168f8b6dc70776650e609672a1874fe2
karthik-pro-engr <101930095+karthik-pro-engr@users.noreply.github.com>
2026-03-30 16:15:04 +0530

    feat(core-testing): add shared MainDispatcherRule for coroutine testing

---
+++ core-testing/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.karthik.pro.engr.github.api.core.testing"
+    compileSdk {
+        version = release(36)
+    }
+
+    defaultConfig {
+        minSdk = 28
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.material)
+
+    implementation(libs.kotlinx.coroutines.test)
+    implementation(libs.junit)
+
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+}
--- core-testing/src/test/java/com/karthik/pro/engr/github/api/core/testing/ExampleUnitTest.kt
+++ core-testing/src/test/java/com/karthik/pro/engr/github/api/core/testing/ExampleUnitTest.kt
@@ -1,7 +1,8 @@
-package com.karthik.pro.engr.github.api.core
+package com.karthik.pro.engr.github.api.core.testing
 
-import org.junit.Assert.assertEquals
 import org.junit.Test
+
+import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
