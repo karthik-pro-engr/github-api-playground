ad749d11ad37d7e41916c16c4c31785a9456d1f0
karthik-pro-engr <101930095+karthik-pro-engr@users.noreply.github.com>
2026-03-31 23:04:43 +0530

    test(repos-ui): add test tag for full screen loader

--- app/src/main/java/com/karthik/pro/engr/github/api/playground/presentation/components/FullScreenLoader.kt
+++ app/src/main/java/com/karthik/pro/engr/github/api/playground/presentation/components/FullScreenLoader.kt
@@ -6,10 +6,16 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import com.karthik.pro.engr.github.api.playground.presentation.repos.GithubRepoListTestTags.FULL_SCREEN_LOADER
 
 @Composable
 fun FullScreenLoader(modifier: Modifier = Modifier) {
-    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+    Box(
+        Modifier
+            .fillMaxSize()
+            .testTag(FULL_SCREEN_LOADER), contentAlignment = Alignment.Center
+    ) {
         CircularProgressIndicator()
     }
 }
