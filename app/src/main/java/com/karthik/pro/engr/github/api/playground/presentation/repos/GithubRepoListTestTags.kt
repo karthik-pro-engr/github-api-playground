e4bdadf071e6175401c69baa372c8a734d1c350b
karthik-pro-engr <101930095+karthik-pro-engr@users.noreply.github.com>
2026-04-01 13:17:00 +0530

    test(repos-ui): add test tag constants for error, list, and pagination states

--- app/src/main/java/com/karthik/pro/engr/github/api/playground/presentation/repos/GithubRepoListTestTags.kt
+++ app/src/main/java/com/karthik/pro/engr/github/api/playground/presentation/repos/GithubRepoListTestTags.kt
@@ -5,4 +5,14 @@ object GithubRepoListTestTags {
     const val SEARCH_BUTTON = "search_button"
 
     const val FULL_SCREEN_LOADER = "full_screen_loader"
+
+    const val FULL_SCREEN_ERROR = "full_screen_error"
+
+    const val FULL_SCREEN_RETRY_BUTTON= "full_screen_retry"
+
+    const val REPO_LIST = "repo_list"
+
+    const val REPO_ITEM = "repo_item"
+
+    const val APPEND_LOADER = "append_loader"
 }
