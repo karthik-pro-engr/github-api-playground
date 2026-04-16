d1130fd4fbd9aead6b8c11384e2ff4dd0d5ae2af
karthik-pro-engr <101930095+karthik-pro-engr@users.noreply.github.com>
2026-04-16 16:31:33 +0530

    feat(repo-detail): add constants for UI test tags in repo detail screen

---
+++ app/src/main/java/com/karthik/pro/engr/github/api/playground/presentation/repo/RepoDetailTestTags.kt
@@ -0,0 +1,27 @@
+package com.karthik.pro.engr.github.api.playground.presentation.repo
+
+object RepoDetailTestTags {
+
+    // Header
+    const val HEADER = "repo_detail_header"
+    const val OWNER = "repo_detail_owner"
+    const val TITLE = "repo_detail_title"
+    const val DESCRIPTION = "repo_detail_description"
+
+
+    // Stats
+    const val STATS = "repo_detail_stats"
+    const val STARS = "repo_detail_stars"
+    const val FORKS = "repo_detail_forks"
+
+    // Language
+    const val LANGUAGE = "repo_detail_language"
+    const val LANGUAGE_LIST = "repo_detail_language_item"
+
+    // Topics
+    const val TOPICS = "repo_detail_topics"
+
+    // Releases
+    const val RELEASE_ITEM = "repo_detail_release_item"
+    const val PROGRESS_INDICATOR = "repo_detail_progress_indicator"
+}
